143aa71b6d92f3d4cd8294e56bab0f1809818873
qsash <50059167+qsash@users.noreply.github.com>
2023-01-31 10:53:36 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,31 @@
+package main
+
+import(
+ "fmt"
+  "database/sql"
+	_ "github.com/go-sql-driver/mysql"
+   "github.com/joho/sqltocsv"
+
+)
+
+
+// connect with db
+func main()  {
+  db, err := sql.Open("mysql", "login:password@tcp(127.0.0.1:8889))/db_name")
+  if err != nil {
+    panic(err)
+  }
+
+ defer db.Close()
+
+ res, _ := db.Query("SELECT * FROM `users...`")
+ //the path to the file
+err = sqltocsv.WriteFile(".../file_name.csv", res)
+if err != nil {
+    panic(err)
+}else{
+  fmt.Println("done")
+}
+
+}
 
